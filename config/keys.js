09547e63de516dceed214451c2357c6cca340169
Imran Rovan <imran04u@yahoo.com>
2020-12-02 18:54:20 +0530

    update db

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://adminUser:UvBwP73Q$7@localhost:27017/?authSource=food'
+  mongoURI: 'mongodb+srv://vembanad:vembanad@food.adhdx.mongodb.net/food?retryWrites=true&w=majority'
 };
 
 
