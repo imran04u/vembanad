c4daea53288a84b052adfeda487a8e065ca377a2
Imran Rovan <imran04u@yahoo.com>
2020-12-11 15:16:19 +0530

    update

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://adminUser:${encodeURIComponent('UvBwP73Q$7')}@127.0.0.1:27017/food'
+  mongoURI: 'mongodb://adminUser:UvBwP73Q$7@127.0.0.1:27017/food'
 };
 
 
