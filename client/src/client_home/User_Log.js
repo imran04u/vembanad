1e256dd83fa83bce4454a62712c5b3e5dc3be939
mdyahya1998 <mdyahya028@gmail.com>
2021-01-07 10:13:56 -0800

    sample

---
+++ client/src/client_home/User_Log.js
@@ -0,0 +1,169 @@
+import jquery from "jquery";
+import React, {useState,useEffect} from "react";
+import ReactDOM from 'react-dom';
+import axios from "axios";
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+//import 'owl.carousel2/dist/assets/owl.carousel.css';
+import $ from 'jquery';
+//import './https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
+import './css/bootstrap.min.css';
+import './css/plugins.css';
+import './css/style.css';
+import './css/responsive.css';
+import logo from './images/logo.png';
+import hmeabout from './images/abtus.png';
+import callus from './images/callus.png';
+import fb from './images/fb.png';
+import insta from './images/insta.png';
+import twitter from './images/twitter.png';
+//import 'owl.carousel2/dist/assets/owl.carousel.js';
+//import 'imports?jQuery=jquery!owl.carousel';
+import {toast} from 'react-toastify';  
+import 'react-toastify/dist/ReactToastify.css';  
+import CONFIG from '../config';
+toast.configure()
+
+function User_Log(props) {
+	const [pass,setPass]=useState("");
+	const [email,setEmail]=useState("");
+	const [msg,setMsg]=useState("");
+	const success = ()=>{  
+		toast("Logged in Successfully") 
+			 
+	  } 
+	  const invalid = ()=>{  
+		toast("Invalid username or password") 
+			 
+	  } 
+	function btn_click(e){
+		e.preventDefault();
+		let data={
+		  email:email,
+		  pass:pass
+		}
+		console.log(data);
+		axios.post(`${CONFIG.baseUrl}/user/login/`, data).then(res=>{
+		// console.log(res.data.result);
+		 
+		 if(res.data.result)
+		 {
+		   // window.location="/cat"
+		  console.log(res);
+		 localStorage.setItem('user',btoa(res.data.email));
+		 localStorage.setItem('address',btoa(res.data.address));
+		 localStorage.setItem('phone',btoa(res.data.phone));
+		 localStorage.setItem('id',btoa(res.data.id));
+		
+		 success()
+		 props.history.push('/')
+		 }
+		 else{
+		
+			invalid()
+		}
+		})
+	  }
+
+    
+
+    return (
+<div id="page">
+		<header>
+			<div class="logo">
+			<a href="/"><img src={logo} class="img-fluid"/></a>
+				<a href="#menu" id="burgernav"><span></span><span></span><span></span></a>
+			</div>
+
+			<div class="headerright">
+				<nav id="menu">
+					<ul>
+					<li><a href="/">Home</a></li>
+						<li><a href="/menu">Menu</a></li>
+						<li><a href="/offers">Offers</a></li>
+						<li><a href="/about">About Us</a></li>
+						<li><a href="/contact">Contact Us</a></li>
+						<li className="active"><a href="/user_log">Login</a></li>
+					</ul>
+				</nav>
+				
+			
+			</div>
+		</header>
+		<section class="innerban subv-1">
+			<h1><span>Login</span></h1>
+		</section>
+		<section class="content register">
+			<div class="container">
+				<div class="loginBlk">
+					<h6>Login</h6>
+					<p>Please login to continue ordering</p>
+
+					<div class="wrap-input">
+					<span class="label-input">Email Id</span>
+						<input class="inputlog" type="text" onChange={(event)=>{setEmail(event.target.value);}} name="Email Id"/>
+						
+					</div>
+					<div class="wrap-input">
+					<span class="label-input">Password</span>
+						<input class="inputlog" type="password" onChange={(event)=>{setPass(event.target.value);}} name="Password"/>
+						
+					</div>            
+					
+					<button class="btns btnslogin" onClick={btn_click}>Login</button>
+					<div class="registerlink">
+						Don't have an account?<a href="/register">Register</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer className="footer">
+			<div className="footer-1 clearfix">
+				<div className="container flx">
+					<div className="footer-contact clearfix">
+						<img src={callus}/>
+						<div className="contact-txt">
+							<p>Call us</p>
+							<p>+974 4412 5928</p>
+						</div>
+					</div>
+					<div className="footer-contact clearfix">
+						<img src={callus}/>
+						<div className="contact-txt">
+							<p>Mail us</p>
+							<p>vembanadqatar@gmail.com</p>
+						</div>
+					</div>
+					<div className="footer-contact clearfix">
+						<ul>
+						<li><a href="#">Privacy Policy</a></li>
+						<li><a href="#">Terms of use</a></li>
+						</ul>
+						</div>
+						<div className="footer-contact clearfix">
+						<ul>
+						<li><a href="/register">Register</a></li>
+						<li><a href="/user_log">Login</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		<div className="footer-2">
+			<div className="container clearfix">
+				<div className="footer-2-txt">
+					<p>© Copyright 2020 Vembanad Restaurant. All rights reserved</p>
+				</div>
+				<div className="footer-social-links">
+					<a href="https://www.facebook.com/thevembanadrestaurant" targer="_blank"><img src={fb} /></a>
+					<a href="https://www.instagram.com/vembanadrestaurant" targer="_blank"><img src={insta} /></a>
+				</div>
+			</div>
+		</div>
+	</footer>	
+	</div>	
+	
+    );
+  }
+  
+  export default User_Log;
