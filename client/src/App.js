838ffd0dc916f0a1f9b05896d2c1fb277223fac0
mdyahya1998 <mdyahya028@gmail.com>
2021-01-09 05:23:15 -0800

    sample

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
  import logo from './logo.svg';
 import './App.css';
-
+import Home from "./client_home/Home";
 
 function App() {
   return (
     <div>
     Hellow world
+    <Home/>
     </div>
   );
 }
