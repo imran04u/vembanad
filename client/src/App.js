8efce777dc8e0467a00e37b5566d511916ae4195
mdyahya1998 <mdyahya028@gmail.com>
2021-01-14 00:12:45 -0800

    sample

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,13 @@ import React from 'react';
 import {BrowserRouter,Route,Switch,Link} from 'react-router-dom'
 
 import Home from "./client_home/Home";
-import Menu from "./client_home/Menu";
+import About from './client_home/About'
+import Contact from './client_home/Contact'
+import user_Dashboard from './client_home/Dashboard'
+import Menu from './client_home/Menu'
+import Offers from './client_home/Offers'
+import Register from './client_home/Register'
+import User_Log from './client_home/User_Log'
 function App() {
   return (
    
@@ -13,8 +19,15 @@ function App() {
 
        <Switch>
      
-    <Route exact path="/" component={Home}/>
+     <Route exact path="/" component={Home}/>
+     <Route exact path="/about" component={About}/>
+     <Route exact path="/contact" component={Contact}/>
+     <Route exact path="/user_dash" component={user_Dashboard}/>
      <Route exact path="/menu" component={Menu}/>
+     <Route exact path="/offers" component={Offers}/>
+     <Route exact path="/register" component={Register}/>
+     <Route exact path="/user_log" component={User_Log}/>
+
       </Switch>
     </div>
    
