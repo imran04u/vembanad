e14faf525fb1c64597b8d3d1d228c01008311e21
mdyahya1998 <mdyahya028@gmail.com>
2021-01-13 22:54:32 -0800

    sample

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
- import logo from './logo.svg';
-import './App.css';
-import Routes from './Routes/Routes';
+//import logo from './logo.svg';
+//import '../../assets/images/logo.png';
 
+import React from 'react';
+
+import {BrowserRouter,Route,Switch,Link} from 'react-router-dom'
+
+import Header from "./client_home/Header";
+import Home from "./client_home/Home";
 
 function App() {
   return (
+   
     <div>
-    Hellow world
-     <Routes />
+    
+
+       <Switch>
+     
+     <Route exact path="/" component={Header}/>
+     <Route exact path="/home" component={Home}/>
+      </Switch>
     </div>
+   
+   
   );
 }
 
