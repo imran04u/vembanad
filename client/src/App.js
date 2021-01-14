1a355dfb201bc891056b99e95ca7b3b1cc5e404e
mdyahya1998 <mdyahya028@gmail.com>
2021-01-13 23:57:45 -0800

    sample

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import {BrowserRouter,Route,Switch,Link} from 'react-router-dom'
 
 import Home from "./client_home/Home";
-
+import Menu from "./client_home/Menu";
 function App() {
   return (
    
@@ -14,6 +14,7 @@ function App() {
        <Switch>
      
     <Route exact path="/" component={Home}/>
+     <Route exact path="/menu" component={Menu}/>
       </Switch>
     </div>
    
