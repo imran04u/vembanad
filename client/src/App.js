856e09710b8ba30d8305646af198f9169a79f7e8
mdyahya1998 <mdyahya028@gmail.com>
2021-01-14 00:23:59 -0800

    sample

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,11 @@ import Menu from './client_home/Menu'
 import Offers from './client_home/Offers'
 import Register from './client_home/Register'
 import User_Log from './client_home/User_Log'
+import ProtectedRouter from './protected'
+import AdminDashboard from './AdminDashboard'
+import Login from './Login'
+
+
 function App() {
   return (
    
@@ -27,6 +32,8 @@ function App() {
      <Route exact path="/offers" component={Offers}/>
      <Route exact path="/register" component={Register}/>
      <Route exact path="/user_log" component={User_Log}/>
+      <Route exact path="/admin" component={Login}/> 
+      <ProtectedRouter exact path='/dash' component={AdminDashboard}/>
 
       </Switch>
     </div>
